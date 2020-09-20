9915b8a3d297eeee3ddb4624e1765f062a4a0e8c
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-20 22:52:32 +0900

    test: return to home screen when LogoIcon click

--- src/display/components/header/header.test.tsx
+++ src/display/components/header/header.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory, MemoryHistory } from 'history';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { alt } from '@/constans';
 import { Header } from '@/display/components';
 
 type RenderTypes = {
@@ -10,7 +11,7 @@ type RenderTypes = {
 };
 
 const rendering = (): RenderTypes => {
-  const history = createMemoryHistory({ initialEntries: ['/'] });
+  const history = createMemoryHistory();
   render(
     <Router history={history}>
       <Header />
@@ -19,6 +20,16 @@ const rendering = (): RenderTypes => {
   const input = screen.getByRole('textbox');
   const searchWord = input.closest('input').value;
   return { history, searchWord };
+};
+
+const renderingOtherThanHome = () => {
+  const history = createMemoryHistory({ initialEntries: ['/any_url'] });
+  render(
+    <Router history={history}>
+      <Header />
+    </Router>
+  );
+  return { history };
 };
 
 const transitionResult = ({ history, searchWord }: RenderTypes) => {
@@ -43,6 +54,17 @@ describe('Header Components', () => {
       const searchIcon = screen.getByTitle('svg');
       fireEvent.click(searchIcon);
       transitionResult({ history, searchWord });
+    });
+  });
+
+  describe('return to home screen', () => {
+    test('LogoIcon click', () => {
+      const { history } = renderingOtherThanHome();
+      const logoIcon = screen.getByAltText(alt);
+      fireEvent.click(logoIcon);
+      const { pathname } = history.location;
+      expect(history.length).toBe(2);
+      expect(`${pathname}`).toBe('/');
     });
   });
 });
