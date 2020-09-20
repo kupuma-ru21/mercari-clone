44486f0bea182e0a2bc14437ce6bbdbb0a7f6857
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-20 19:41:46 +0900

    test: screen transition when searchIcon click

--- src/display/components/header/header.test.tsx
+++ src/display/components/header/header.test.tsx
@@ -4,23 +4,42 @@ import { createMemoryHistory } from 'history';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Header } from '@/display/components';
 
-const history = createMemoryHistory();
-render(
-  <Router history={history}>
-    <Header />
-  </Router>
-);
+const rendering = () => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  render(
+    <Router history={history}>
+      <Header />
+    </Router>
+  );
+  return { history };
+};
 
 describe('Header Components', () => {
-  test('screen transition when search', () => {
-    const input = screen.getByRole('textbox');
-    const form = screen.getByRole('form');
-    fireEvent.submit(form);
-    const { pathname, search } = history.location;
-    const searchWord = input.closest('input').value;
-    expect(history.length).toBe(2);
-    expect(`${pathname}${search}${searchWord}`).toBe(
-      `/search?keyword=${searchWord}`
-    );
+  describe('screen transition when search', () => {
+    test('form submit', () => {
+      const { history } = rendering();
+      const input = screen.getByRole('textbox');
+      const form = screen.getByRole('form');
+      fireEvent.submit(form);
+      const { pathname, search } = history.location;
+      const searchWord = input.closest('input').value;
+      expect(history.length).toBe(2);
+      expect(`${pathname}${search}${searchWord}`).toBe(
+        `/search?keyword=${searchWord}`
+      );
+    });
+
+    test('searchIcon click', () => {
+      const { history } = rendering();
+      const input = screen.getByRole('textbox');
+      const searchIcon = screen.getByTitle('svg');
+      fireEvent.click(searchIcon);
+      const { pathname, search } = history.location;
+      const searchWord = input.closest('input').value;
+      expect(history.length).toBe(2);
+      expect(`${pathname}${search}${searchWord}`).toBe(
+        `/search?keyword=${searchWord}`
+      );
+    });
   });
 });
