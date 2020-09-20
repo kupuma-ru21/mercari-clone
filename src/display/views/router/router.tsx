301e10155bc4fa977b0138b9d7076677e8f513fd
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-20 20:02:01 +0900

    chore: organize directories

--- src/display/views/router/router.tsx
+++ src/display/views/router/router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Home } from '@/display';
+import { Home } from '@/display/views';
 
 const Router: React.FC = () => {
   return (
