5f36d67db8af2a4e7342a6632f9ea6b496450600
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-20 20:11:22 +0900

    feat: create search component

--- src/display/views/router/router.tsx
+++ src/display/views/router/router.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Home } from '@/display/views';
+import { Home, Search } from '@/display/views';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/search" exact component={Search} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/display/views/search/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from '@/display/components';
+
+const Search: React.FC = () => {
+  return (
+    <>
+      <Header />
+      Search
+    </>
+  );
+};
+
+export default Search;
