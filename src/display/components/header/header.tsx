b9b92baac6886706180acde5b2492d2010e2f604
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-20 17:03:25 +0900

    feat: screen transition when search

---
+++ src/display/components/header/header.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import useReactRouter from 'use-react-router';
+import { TextField } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import { src, alt, placeholder } from '@/constans';
+import Styles from './header-styles.scss';
+
+const Header: React.FC = () => {
+  const { history } = useReactRouter();
+  const [state, setState] = useState({ search: '' });
+  const onChange = (event): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const onSubmit = (): void => history.push(`/search?keyword=${state.search}`);
+
+  return (
+    <div className={Styles.headerWrap}>
+      <img src={src} alt={alt} />
+      <form aria-label="form" onSubmit={onSubmit}>
+        <TextField
+          variant="outlined"
+          fullWidth
+          size="small"
+          placeholder={placeholder}
+          name="search"
+          value={state.search}
+          onChange={onChange}
+        />
+      </form>
+      <SearchIcon className={Styles.icon} />
+    </div>
+  );
+};
+
+export default Header;
