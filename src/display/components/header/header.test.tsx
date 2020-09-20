b2bd85afd8591e5aee2c05e0df288bd1198a551e
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-20 19:43:44 +0900

    refactor: header test

--- src/display/components/header/header.test.tsx
+++ src/display/components/header/header.test.tsx
@@ -1,45 +1,48 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, MemoryHistory } from 'history';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Header } from '@/display/components';
 
-const rendering = () => {
+type RenderTypes = {
+  history: MemoryHistory;
+  searchWord: string;
+};
+
+const rendering = (): RenderTypes => {
   const history = createMemoryHistory({ initialEntries: ['/'] });
   render(
     <Router history={history}>
       <Header />
     </Router>
   );
-  return { history };
+  const input = screen.getByRole('textbox');
+  const searchWord = input.closest('input').value;
+  return { history, searchWord };
+};
+
+const transitionResult = ({ history, searchWord }: RenderTypes) => {
+  const { pathname, search } = history.location;
+  expect(history.length).toBe(2);
+  expect(`${pathname}${search}${searchWord}`).toBe(
+    `/search?keyword=${searchWord}`
+  );
 };
 
 describe('Header Components', () => {
   describe('screen transition when search', () => {
     test('form submit', () => {
-      const { history } = rendering();
-      const input = screen.getByRole('textbox');
+      const { history, searchWord } = rendering();
       const form = screen.getByRole('form');
       fireEvent.submit(form);
-      const { pathname, search } = history.location;
-      const searchWord = input.closest('input').value;
-      expect(history.length).toBe(2);
-      expect(`${pathname}${search}${searchWord}`).toBe(
-        `/search?keyword=${searchWord}`
-      );
+      transitionResult({ history, searchWord });
     });
 
-    test('searchIcon click', () => {
-      const { history } = rendering();
-      const input = screen.getByRole('textbox');
+    test('serchIcon click', () => {
+      const { history, searchWord } = rendering();
       const searchIcon = screen.getByTitle('svg');
       fireEvent.click(searchIcon);
-      const { pathname, search } = history.location;
-      const searchWord = input.closest('input').value;
-      expect(history.length).toBe(2);
-      expect(`${pathname}${search}${searchWord}`).toBe(
-        `/search?keyword=${searchWord}`
-      );
+      transitionResult({ history, searchWord });
     });
   });
 });
