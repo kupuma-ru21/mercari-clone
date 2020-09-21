7bf58c1d687120d8dad7b30a4fa29cd76f0c1b96
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-21 22:10:18 +0900

    feat: login button mouseOver and mouseOut

--- src/display/components/header/header-bottom-right/header-bottom-right.tsx
+++ src/display/components/header/header-bottom-right/header-bottom-right.tsx
@@ -1,18 +1,32 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
 import { Button } from '@material-ui/core';
 import Styles from './header-bottom-right-style.scss';
 
+type Variant = 'text' | 'outlined' | 'contained';
+
 const HeaderBottomRight: React.FC = () => {
+  const [loginVariant, setLoginVariant] = useState<Variant>('outlined');
+  const loginMouseOver = (): void => setLoginVariant('contained');
+  const loginMouseOut = (): void => setLoginVariant('outlined');
+
   return (
     <div className={Styles.rightSide}>
-      <Button variant="contained" color="secondary" size="small">
+      <Button
+        title="memberRegister"
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
         新規会員登録
       </Button>
       <Button
+        title="login"
         className={Styles.login}
-        variant="outlined"
+        variant={loginVariant}
         color="primary"
         size="small"
+        onMouseOver={loginMouseOver}
+        onMouseOut={loginMouseOut}
       >
         ログイン
       </Button>
