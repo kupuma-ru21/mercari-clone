eb2b9f2e840b116d7bb3a8ce662f1667bc9e3fb5
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-21 23:47:36 +0900

    feat: searchByCategory click category page display

--- src/display/components/header/modal/modal.tsx
+++ src/display/components/header/modal/modal.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import useReactRouter from 'use-react-router';
 import { Props } from '@/types/modal';
 import Styles from './modal-style.scss';
 
 const Modal: React.FC<Props> = ({ elInfo }: Props) => {
   const [displayFlg, setDisplayFlg] = useState(false);
   const onMouse = (): void => setDisplayFlg(!displayFlg);
+  const { history } = useReactRouter();
+  const transaction = (): void => history.push('/category');
 
   return (
     <div>
-      <div role="heading" onMouseOver={onMouse} onMouseOut={onMouse}>
+      <div
+        role="heading"
+        onMouseOver={onMouse}
+        onMouseOut={onMouse}
+        onClick={transaction}
+      >
         {elInfo.el}
       </div>
       <div role="directory">
