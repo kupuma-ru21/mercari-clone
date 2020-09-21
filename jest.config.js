a1f8bff24f5c80f343ddeacb83639a3c155c46ff
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-21 16:33:26 +0900

    chore: add @testing-library/jest-dom

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}", "!**/*.d.ts"],
-  coverageDirectory: "coverage",
-  testEnvironment: "jsdom",
-  transform: { ".+\\.(ts|tsx)$": "ts-jest" },
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['<rootDir>/src/main/config/jest-setup.ts'],
+  testEnvironment: 'jsdom',
+  transform: { '.+\\.(ts|tsx)$': 'ts-jest' },
   moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1",
-    "\\.scss$": "identity-obj-proxy",
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy',
   },
 };
