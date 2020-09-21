c72a5f9220ce73b1bfe71d543fe8e3fb8d4022e0
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-21 10:57:41 +0900

    feat: create layout at the bottom of the header

--- src/display/components/header/index.tsx
+++ src/display/components/header/index.tsx
@@ -1,11 +1,12 @@
 import React, { memo } from 'react';
-import { HeaderTop } from '@/display/components';
+import { HeaderTop, HeaderBottom } from '@/display/components';
 import * as Styles from './header-style.scss';
 
 const Header: React.FC = () => {
   return (
     <div className={Styles.headerWrap}>
       <HeaderTop />
+      <HeaderBottom />
     </div>
   );
 };
--- src/display/components/index.ts
+++ src/display/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from './header';
 export { default as HeaderTop } from './header/header-top/header-top';
+export { default as HeaderBottom } from './header/header-bottom/header-bottom';
