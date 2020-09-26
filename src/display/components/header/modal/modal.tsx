4dc1d8476adfb114e528ae37bf13a1cb01eb7f76
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-26 12:03:05 +0900

    feat: modalCategoryList display control check

--- src/display/components/header/modal/modal.tsx
+++ src/display/components/header/modal/modal.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useReactRouter from 'use-react-router';
 import { Props, ModalContent } from '@/types/modal';
 import { ModalList } from '@/display/components';
 import Styles from './modal-style.scss';
 
 const Modal: React.FC<Props> = ({ elInfo }: Props) => {
+  const [displayFlg, setDisplayFlg] = useState(false);
   const { history } = useReactRouter();
   const transaction = (): void => history.push(`/${elInfo.name}`);
+  const modalDisplayControl = (): void => setDisplayFlg(!displayFlg);
 
   return (
-    <div className={Styles.modalWrap}>
+    <div
+      role="directory"
+      className={Styles.modalWrap}
+      onMouseEnter={modalDisplayControl}
+      onMouseLeave={modalDisplayControl}
+    >
       <div role="heading" className={Styles.head} onClick={transaction}>
         {elInfo.el}
       </div>
       <ul>
-        {elInfo.modalList.map((item: ModalContent) => (
-          <ModalList itemInfo={{ item, elInfo }} key={item.id} />
-        ))}
+        {displayFlg &&
+          elInfo.modalList.map((item: ModalContent) => (
+            <ModalList itemInfo={{ item, elInfo }} key={item.id} />
+          ))}
       </ul>
     </div>
   );
