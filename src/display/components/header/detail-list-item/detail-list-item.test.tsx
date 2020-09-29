dc9a5e1ef7308bdcc21bafa0a4c7bd3c2a8f679c
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-29 22:40:09 +0900

    test: categoryDetail ListItem background color switching

---
+++ src/display/components/header/detail-list-item/detail-list-item.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { categoryArray, categoryDetail } from '@/constans/header';
+import { ModalContent } from '@/types/header/modal';
+import { ModalContext } from '@/contexts';
+import { DetailListItem } from '@/display/components';
+
+const rendering = (item: ModalContent): void => {
+  const history = createMemoryHistory();
+  render(
+    <ModalContext.Provider value={{ state: {}, setState: () => jest.fn() }}>
+      <Router history={history}>
+        <DetailListItem item={item} />
+      </Router>
+    </ModalContext.Provider>
+  );
+};
+
+describe('ModalDetailList Components', () => {
+  test('categoryDetail ListItem background color switching test', () => {
+    categoryArray.forEach((categoryListItemItem: ModalContent) => {
+      categoryDetail[`categoryDetail${categoryListItemItem.id}`].forEach(
+        (detailListItem: ModalContent, index: number) => {
+          rendering(detailListItem);
+          const list = screen.getAllByRole('listitem');
+          expect(list[index]).not.toHaveClass('hoverListItem');
+          fireEvent.mouseEnter(list[index]);
+          expect(list[index]).toHaveClass('hoverListItem');
+          fireEvent.mouseLeave(list[index]);
+          expect(list[index]).not.toHaveClass('hoverListItem');
+        }
+      );
+    });
+  });
+});
