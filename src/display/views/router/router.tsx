55f95b724eafc594b635ca0f2a669b60dec34af6
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-30 22:10:23 +0900

    chore: remove unnecessary import

--- src/display/views/router/router.tsx
+++ src/display/views/router/router.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Home, Search } from '@/display/views';
+import { Home } from '@/display/views';
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/search" exact component={Search} />
       </Switch>
     </BrowserRouter>
   );
