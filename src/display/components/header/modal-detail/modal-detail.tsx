a5932330ea162a46bc460c676186482439b39ccd
kupuma-ru21 <kupumaru21@gmail.com>
2020-09-30 20:48:22 +0900

    chore: layout adjusment

--- src/display/components/header/modal-detail/modal-detail.tsx
+++ src/display/components/header/modal-detail/modal-detail.tsx
@@ -3,12 +3,13 @@ import { ModalContext } from '@/contexts';
 import { ModalContent } from '@/types/header/modal';
 import { categoryDetail } from '@/constans/header';
 import { DetailListItem } from '@/display/components';
+import Styles from './modal-detail-style.scss';
 
 const ModalDetail: React.FC = () => {
   const { state } = useContext(ModalContext);
   const detailkey = `categoryDetail${state.listItemID}`;
   return (
-    <ul>
+    <ul className={Styles.modalDetailWrap}>
       {categoryDetail[detailkey].map((item: ModalContent) => (
         <DetailListItem key={item.id} item={item} />
       ))}
