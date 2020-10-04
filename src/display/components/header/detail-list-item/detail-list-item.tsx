8e163fa5886780319a8d24065fc4ad65abd8eae6
kupuma-ru21 <kupumaru21@gmail.com>
2020-10-04 17:18:48 +0900

    feat: detailListItemClick

--- src/display/components/header/detail-list-item/detail-list-item.tsx
+++ src/display/components/header/detail-list-item/detail-list-item.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import useReactRouter from 'use-react-router';
 import { ModalContent } from '@/types/header/modal';
 import Styles from './detail-list-item-style.scss';
 
@@ -13,11 +14,17 @@ const DetailListItem: React.FC<Props> = ({ item }) => {
   };
   const onMouseLeave = (): void => setClassName(Styles.listItem);
 
+  const { history } = useReactRouter();
+  const clickDetailItem = (): void => {
+    history.push(`/category/${item.id}`);
+  };
+
   return (
     <li
       className={className}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      onClick={clickDetailItem}
     >
       {item.text}
     </li>
